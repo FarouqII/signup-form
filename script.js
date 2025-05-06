b9cd8d518042795b304b8547fe1d3c3f3d9bc8f4
FarouqII <farouq006@gmail.com>
2025-05-06 15:21:01 +0300

    Add owl
    Added an owl with blinking animation to give life to the page

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-window.onload = function () {
-    setTimeout(function () {
-      document.getElementById('loading-page').style.display = 'none';
-    }, 3000);
+const logo = document.getElementById('logo');
+const frame = document.getElementById('frame');
+
+// Loading Screen
+window.onload = () => {
+    setTimeout(() => {
+      logo.style.transform = "translate(-135px, 175px) scale(0.5)";
+      frame.style.opacity = '1';
+    }, 2000);
   };
-  
+
+// Owl Animation
+frame.src = "./assets/owl-animation/pixil-frame-0.png";
+
+function blink() {
+  frame.src = "./assets/owl-animation/pixil-frame-1.png"
+
+  setTimeout(() => {
+    frame.src = "./assets/owl-animation/pixil-frame-0.png"
+
+    const nextBlink = Math.floor(Math.random() * 4000) + 2000;
+    setTimeout(blink, nextBlink);
+  }, 150)
+}
+setTimeout(blink, 2000);
